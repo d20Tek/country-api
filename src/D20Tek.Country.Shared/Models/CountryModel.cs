06fc2abefb96a486cbb4069bd0ef9400e6628332
Pedro Silva <pedrosi@microsoft.com>
2023-04-10 11:15:02 -0700

    Created initial webapi project and shared project for models.

---
+++ src/D20Tek.Country.Shared/Models/CountryModel.cs
@@ -0,0 +1,18 @@
+﻿//---------------------------------------------------------------------------------------------------------------------
+// Copyright (c) d20Tek.  All rights reserved.
+//---------------------------------------------------------------------------------------------------------------------
+namespace D20Tek.Country.Shared.Models
+{
+    public class CountryModel
+    {
+        public string Name { get; set; } = string.Empty;
+
+        public string OfficialName { get; set; } = string.Empty;
+
+        public string Alpha2Code { get; set; } = string.Empty;
+
+        public string Alpha3Code { get; set; } = string.Empty;
+
+        public string NumericCode { get; set; } = string.Empty;
+    }
+}
---
+++ src/D20Tek.CountryApi/Controllers/CountriesController.cs
@@ -0,0 +1,44 @@
+﻿//---------------------------------------------------------------------------------------------------------------------
+// Copyright (c) d20Tek.  All rights reserved.
+//---------------------------------------------------------------------------------------------------------------------
+using Microsoft.AspNetCore.Mvc;
+
+namespace D20Tek.CountryApi.Controllers
+{
+    [Route("api/v1/countries")]
+    [ApiController]
+    public class CountriesController : ControllerBase
+    {
+        // GET: api/countries
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/countries/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/countries
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/countries/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/countries/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
