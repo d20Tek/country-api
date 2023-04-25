78cd8b37c44c5c61a00133d279afb34898637222
Pedro Silva <pedrosi@microsoft.com>
2023-04-25 16:23:50 -0700

    Added unit tests for the Services.Core project.

---
+++ tests/D20Tek.Services.Core.UnitTests/MemoryRepositoryTests.cs
@@ -0,0 +1,69 @@
+﻿//---------------------------------------------------------------------------------------------------------------------
+// Copyright (c) d20Tek.  All rights reserved.
+//---------------------------------------------------------------------------------------------------------------------
+using D20Tek.Services.Core.UnitTests.Fakes;
+
+namespace D20Tek.Services.Core.UnitTests
+{
+    [TestClass]
+    public class MemoryRepositoryTests
+    {
+        [TestMethod]
+        public async Task CreateItem_WithValidEntity()
+        {
+            // arrange
+            var newEntity = new TestEntity { EntityId = "foo", Name = "bar", Value = 3 };
+            var repo = new TestRepository();
+
+            // act
+            var result = await repo.CreateItemAsync(newEntity);
+
+            // assert
+            Assert.IsNotNull(result);
+            Assert.AreEqual("foo", result.EntityId);
+        }
+
+        [TestMethod]
+        public async Task UpdateItem_WithValidEntity()
+        {
+            // arrange
+            var entities = new List<TestEntity>
+            {
+                new TestEntity { EntityId = "test1", Name = "Test1", Value = 1 },
+            };
+            var repo = new TestRepository(entities);
+
+            var updatedEntity = new TestEntity { EntityId = "test1", Name = "bar", Value = 3 };
+
+            // act
+            var result = await repo.UpdateItemAsync(updatedEntity);
+
+            // assert
+            Assert.IsNotNull(result);
+            Assert.AreEqual("test1", result.EntityId);
+            Assert.AreEqual("bar", result.Name);
+            Assert.AreEqual(3, result.Value);
+        }
+
+        [TestMethod]
+        public async Task DeleteItem_WithValidEntity()
+        {
+            // arrange
+            var entities = new List<TestEntity>
+            {
+                new TestEntity { EntityId = "test1", Name = "Test1", Value = 1 },
+            };
+            var repo = new TestRepository(entities);
+
+            // act
+            var result = await repo.DeleteItemAsync("test1");
+
+            // assert
+            Assert.IsNotNull(result);
+            Assert.AreEqual("test1", result.EntityId);
+
+            var items = await repo.GetItemsAsync();
+            Assert.IsFalse(items.Any());
+        }
+    }
+}
