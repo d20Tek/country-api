48b760defeffa5bd1bdde20c041af087f0d12164
Pedro Silva <pedrosi@microsoft.com>
2023-04-25 16:47:53 -0700

    Added exceptions for basic operations: not found, already exists.

---
+++ src/D20Tek.Services.Core/EntityAlreadyExistsException.cs
@@ -0,0 +1,19 @@
+﻿//---------------------------------------------------------------------------------------------------------------------
+// Copyright (c) d20Tek.  All rights reserved.
+//---------------------------------------------------------------------------------------------------------------------
+namespace D20Tek.Services.Core
+{
+    public class EntityAlreadyExistsException : Exception
+    {
+        public EntityAlreadyExistsException(string entityIdName, object entityIdValue, Exception? innerException = null)
+            : base($"Entity with {entityIdName} = {entityIdValue} already exists in this repository.", innerException)
+        {
+            EntityIdName = entityIdName;
+            EntityIdValue = entityIdValue;
+        }
+
+        public string EntityIdName { get; init; }
+
+        public object EntityIdValue { get; init; }
+    }
+}
---
+++ tests/D20Tek.Services.Core.UnitTests/ExceptionTests.cs
@@ -0,0 +1,41 @@
+﻿//---------------------------------------------------------------------------------------------------------------------
+// Copyright (c) d20Tek.  All rights reserved.
+//---------------------------------------------------------------------------------------------------------------------
+namespace D20Tek.Services.Core.UnitTests
+{
+    [TestClass]
+    public class ExceptionTests
+    {
+        [TestMethod]
+        public void EntityAlreadyExistsException_Create()
+        {
+            // arrange
+
+            // act
+            var ex = new EntityAlreadyExistsException("Id", "1234");
+
+            // assert
+            Assert.IsNotNull(ex);
+            Assert.AreEqual(ex.EntityIdName, "Id");
+            Assert.AreEqual(ex.EntityIdValue, "1234");
+            StringAssert.Contains(ex.Message, "Id");
+            StringAssert.Contains(ex.Message, "1234");
+        }
+
+        [TestMethod]
+        public void EntityNotFoundException_Create()
+        {
+            // arrange
+
+            // act
+            var ex = new EntityNotFoundException("Id", "1234");
+
+            // assert
+            Assert.IsNotNull(ex);
+            Assert.AreEqual(ex.EntityIdName, "Id");
+            Assert.AreEqual(ex.EntityIdValue, "1234");
+            StringAssert.Contains(ex.Message, "Id");
+            StringAssert.Contains(ex.Message, "1234");
+        }
+    }
+}
